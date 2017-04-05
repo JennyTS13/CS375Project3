9d7a3ef160e9b8fbb8055efd04224e89f4d3f5fd
Tiffany Lam <tclam05@gmail.com>
2017-04-05 16:47:14 -0400

    minor change

--- src/StrassenForkJoin.java
+++ src/StrassenForkJoin.java
@@ -11,5 +11,18 @@
  * The algorithm uses Java's Fork/Join framework to compute the
  * product of two matrices using the Strassen's Algorithm.
  */
-public class StrassenForkJoin {
+public class StrassenForkJoin implements MatrixMult {
+    /**
+     * Computes the result of multiplying two 2D arrays
+     *
+     * @param   A the first 2D array
+     * @param   B the second 2D array
+     * @return  the resulting multiplication matrix
+     */
+    @Override
+    public int[][] computeMatrixMult(int[][] A, int[][] B) {
+        int[][] result = new int[A.length][A.length];
+        return result;
+    }
+
 }
