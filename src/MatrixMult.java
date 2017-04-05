084e2bd247ef3b1aaa2bb56ba81f67b550a76b83
Tiffany Lam <tclam05@gmail.com>
2017-04-05 16:30:11 -0400

    MatrixMult interface

---
+++ src/MatrixMult.java
@@ -0,0 +1,11 @@
+/*
+ * File:    MatrixMult.java
+ * Authors: Charlie Beck, Phoebe Hughes, Tiffany Lam, Jenny Lin
+ * Date:    April 10, 2017
+ * Project: 3
+ */
+
+public interface MatrixMult {
+
+    public int[][] computeMatrixMult(int[][] A, int[][] B);
+}
