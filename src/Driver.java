8ac7b1521019ddbd2a899e4627d78186bba6ed43
Tiffany Lam <tclam05@gmail.com>
2017-04-05 14:36:10 -0400

    added driver for testing

---
+++ src/Driver.java
@@ -0,0 +1,13 @@
+/*
+ * File:    Driver.java
+ * Authors: Charlie Beck, Phoebe Hughes, Tiffany Lam, Jenny Lin
+ * Date:    April 10, 2017
+ * Project: 3
+ */
+
+/**
+ * A driver for testing different algorithms of multiplying two matrices.
+ */
+public class Driver {
+
+}
