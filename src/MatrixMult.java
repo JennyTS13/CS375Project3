5ee943797c089c97916bc107d5e2b96db9523584
Tiffany Lam <tclam05@gmail.com>
2017-04-05 17:01:51 -0400

    added javadocs

--- src/MatrixMult.java
+++ src/MatrixMult.java
@@ -5,7 +5,17 @@
  * Project: 3
  */
 
+/**
+ * An interface used for matrix multiplication
+ */
 public interface MatrixMult {
 
+    /**
+     * Computes the result of multiplying two 2D arrays
+     *
+     * @param   A the first 2D array
+     * @param   B the second 2D array
+     * @return  the resulting multiplication matrix
+     */
     public int[][] computeMatrixMult(int[][] A, int[][] B);
 }
