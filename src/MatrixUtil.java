d7a4e480878df7291fe8ae24ed3d86a6c5442613
Tiffany Lam <tclam05@gmail.com>
2017-04-05 17:28:14 -0400

    formatting

--- src/MatrixUtil.java
+++ src/MatrixUtil.java
@@ -17,10 +17,10 @@ public class MatrixUtil {
      * @param   B the second matrix
      * @return  Sum of the two input matrices
      */
-    public static int[][] addMatrices(int[][] A, int[][] B){
+    public static int[][] addMatrices(int[][] A, int[][] B) {
         int[][] result = new int[A.length][A[0].length];
-        for(int i = 0; i < A.length; i++){
-            for(int j = 0; j < A[0].length; j++){
+        for(int i = 0; i < A.length; i++) {
+            for(int j = 0; j < A[0].length; j++) {
                 result[i][j] = A[i][j] + B[i][j];
             }
         }
@@ -34,10 +34,10 @@ public class MatrixUtil {
      * @param   B the second matrix
      * @return  Difference of the two input matrices (A - B)
      */
-    public static int[][] subtractMatrices(int[][] A, int[][] B){
+    public static int[][] subtractMatrices(int[][] A, int[][] B) {
         int[][] result = new int[A.length][A[0].length];
-        for(int i = 0; i < A.length; i++){
-            for(int j = 0; j < A[0].length; j++){
+        for(int i = 0; i < A.length; i++) {
+            for(int j = 0; j < A[0].length; j++) {
                 result[i][j] = A[i][j] - B[i][j];
             }
         }
@@ -69,7 +69,7 @@ public class MatrixUtil {
      *
      * @param matrix the given matrix
      */
-    public static void printMatrix(int[][] matrix){
+    public static void printMatrix(int[][] matrix) {
         for(int i = 0; i < matrix.length; i++){
             for(int j = 0; j < matrix[0].length; j++) {
                 System.out.print(matrix[i][j] + " ");
@@ -88,7 +88,7 @@ public class MatrixUtil {
      * @return  the size of the matrix
      *          that satisfies the 2^n size requirement of Strassen's
      */
-    public static int calcSize(int rows, int cols){
+    public static int calcSize(int rows, int cols) {
         int newRows, newCols;
         double rowExp = Math.log(rows)/Math.log(2);
         double colExp = Math.log(cols)/Math.log(2);
@@ -104,7 +104,7 @@ public class MatrixUtil {
      * @param   A 2D matrix to divide into fourths
      * @return  3D array holding the 4 quarters of the input matrix
      */
-    public static int[][][] subDivideMatrix(int[][] A){
+    public static int[][][] subDivideMatrix(int[][] A) {
         int subMatrixSize = A.length/2;
         int [][][] subMatrices = new int[4][subMatrixSize][subMatrixSize];
 
@@ -127,7 +127,7 @@ public class MatrixUtil {
      * @param   dimension integer of desired matrix dimension
      * @return  2D matrix of the input matrix padded with 0's
      */
-    public static int[][] padMatrixZeroes(int[][] A, int dimension){
+    public static int[][] padMatrixZeroes(int[][] A, int dimension) {
         int[][] result = new int[dimension][dimension];
         for (int i = 0; i < dimension; i++) {
             for (int j = 0; j < dimension; j++) {
